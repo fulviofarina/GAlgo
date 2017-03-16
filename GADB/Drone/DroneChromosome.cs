1a24c6fc2adb7946135cbd7bdf9c213d3222b6f8
fulviofarina <fulviofarina@gmail.com>
2017-03-15 20:20:28 -0400

    v1.0

---
+++ GADB/Drone/DroneChromosome.cs
@@ -0,0 +1,65 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Randomizations;
+
+namespace GADB
+{
+    public sealed class DroneChromosome : ChromosomeBase
+    {
+        private int numberOfGenes;
+
+        // private HashSet<int> nonRepeated;
+        private int maxEmpty = 0;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sizeOfChromosome"></param>
+        /// <param name="numOfGenes"></param>
+        public DroneChromosome(int sizeOfChromosome, int numOfGenes, int maxJunk) : base(sizeOfChromosome)
+        {
+            // nonRepeated = new HashSet<int>();
+
+            numberOfGenes = numOfGenes; // do I need the values? nope I think, only indexes
+
+            maxEmpty = maxJunk;
+            var citiesIndexes = RandomizationProvider.Current.GetUniqueInts(numberOfGenes, 1, numberOfGenes + 1);
+
+            for (int i = 0; i < numberOfGenes; i++)
+            {
+                int valor = citiesIndexes[i];
+                //   if (valor > numberOfGenes) valor = -1;
+                Gene g = new Gene(valor);
+                ReplaceGene(i, g);
+            }
+        }
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            int valor = RandomizationProvider.Current.GetInt(1, numberOfGenes + 1);
+            //   if (valor > numberOfGenes) valor = -1;
+            return new Gene(valor);
+        }
+
+
+        /// <summary>
+        /// Creates a new chromosome using the same structure of this.
+        /// </summary>
+        /// <returns>The new chromosome.</returns>
+        public override IChromosome CreateNew()
+        {
+            return new DroneChromosome(this.Length, this.numberOfGenes, this.maxEmpty);
+        }
+
+        /// <summary>
+        /// Creates a clone.
+        /// </summary>
+        /// <returns>The chromosome clone.</returns>
+        public override IChromosome Clone()
+        {
+             DroneChromosome c = base.Clone() as DroneChromosome;
+            c.numberOfGenes = this.numberOfGenes;
+            c.maxEmpty = this.maxEmpty;
+            return c;
+        }
+    }
+}
---
+++ GADB/Func/FuncChromosome.cs
@@ -0,0 +1,63 @@
+﻿using GeneticSharp.Domain.Chromosomes;
+using GeneticSharp.Domain.Randomizations;
+
+namespace GADB
+{
+    public sealed class FuncChromo : ChromosomeBase
+    {
+        private int numberOfGenes;
+
+        // private HashSet<int> nonRepeated;
+        private int maxEQNumber = 0;
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="sizeOfChromosome"></param>
+        /// <param name="numOfGenes"></param>
+        public FuncChromo(int numOfGenes, int numberEqs) : base(numOfGenes + 1)
+        {
+            // nonRepeated = new HashSet<int>();
+
+            numberOfGenes = numOfGenes; // do I need the values? nope I think, only indexes
+
+            maxEQNumber = numberEqs;
+
+            for (int i = 0; i < numberOfGenes + 1; i++)
+            {
+                Gene g = GenerateGene(i);
+                ReplaceGene(i, g);
+            }
+        }
+
+        public override Gene GenerateGene(int geneIndex)
+        {
+            object o = 0;
+            if (geneIndex == 0) o = RandomizationProvider.Current.GetInt(0, maxEQNumber);
+            else o = RandomizationProvider.Current.GetDouble(-1, 1);
+
+            return new Gene(o);
+        }
+
+        /// <summary>
+        /// Creates a new chromosome using the same structure of this.
+        /// </summary>
+        /// <returns>The new chromosome.</returns>
+        public override IChromosome CreateNew()
+        {
+            return new FuncChromo(this.numberOfGenes, this.maxEQNumber);
+        }
+
+        /// <summary>
+        /// Creates a clone.
+        /// </summary>
+        /// <returns>The chromosome clone.</returns>
+        public override IChromosome Clone()
+        {
+            FuncChromo c = base.Clone() as FuncChromo;
+            c.numberOfGenes = this.numberOfGenes;
+            c.maxEQNumber = this.maxEQNumber;
+            return c;
+        }
+    }
+}
